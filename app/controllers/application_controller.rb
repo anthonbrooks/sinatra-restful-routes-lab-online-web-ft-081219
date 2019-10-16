6392c3f8314cc70f1660db1174eea532ef72a841
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-16 17:40:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
-
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index 
+  end 
+  
+  get '/recipes/new' do
+    erb :'/recipes/new'
+  end
+  
+  get '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :'recipes/show'
+  end 
+  
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :'/recipes/edit'
+  end
+  
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save 
+    redirect "/recipes/#{ @recipe.id }"
+  end
+  
+  post '/recipes' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.save
+    redirect "/recipes/#{ @recipe.id }"
+  end
+  
+  delete '/recipes/:id' do
+    @review = Review.find_by_id(params[:id]).destroy
+    redirect '/index'
+  end
 end
